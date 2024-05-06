4d9307202561bd3cbd2baa052e106a00e3ff025b
KIMGANGYEON <rkddus3177@naver.com>
2024-05-06 21:28:32 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://kimgangyeon:abcde12345@cluster0.dmm90qt.mongodb.net",
-    {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connect..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.json({ success: false, err });
+  }
 });
 
 app.listen(port, () => {
